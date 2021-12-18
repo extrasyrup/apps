a73c93ca729c04a54266a4e33e7d894dea078a03
Extra Syrup <iamextrasyrup@gmail.com>
2021-12-18 11:34:12 -0500

    Latest.

--- google-places/app.js
+++ google-places/app.js
@@ -6,12 +6,11 @@ const apiMaxPages = 3;
 const baseURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
 const key = 'AIzaSyDFn08wR53O-_nEOB52cxwgBIvjR5ktKkc';
 let apiAfter = '', finalResult = [];
-//let location = '39.5983379,-86.1199478'; //Greenwood, IN
+let location = '39.5983379,-86.1199478'; //Greenwood, IN
 //let location = '39.7797003,-86.2728328'; //Indianapolis, IN
-let location = '39.1564772,-75.5484393'; //Dover, DE
+//let location = '39.1564772,-75.5484393'; //Dover, DE
 let radius = '8000'; //In meters
-//let types = ['accounting','airport','amusement_park','aquarium','art_gallery','atm','bakery','bank','bar','beauty_salon','bicycle_store','book_store','bowling_alley','bus_station','cafe','campground','car_dealer','car_rental','car_repair','car_wash','casino','cemetery','church','city_hall','clothing_store','convenience_store','courthouse','dentist','department_store','doctor','drugstore','electrician','electronics_store','embassy','fire_station','florist','funeral_home','furniture_store','gas_station','gym','hair_care','hardware_store','hindu_temple','home_goods_store','hospital','insurance_agency','jewelry_store','laundry','lawyer','library','light_rail_station','liquor_store','local_government_office','locksmith','lodging','meal_delivery','meal_takeaway','mosque','movie_rental','movie_theater','moving_company','museum','night_club','painter','park','parking','pet_store','pharmacy','physiotherapist','plumber','police','post_office','primary_school','real_estate_agency','restaurant','roofing_contractor','rv_park','school','secondary_school','shoe_store','shopping_mall','spa','stadium','storage','store','subway_station','supermarket','synagogue','taxi_stand','tourist_attraction','train_station','transit_station','travel_agency','university','veterinary_care','zoo'];
-let types = ['hospital', 'physiotherapist', 'doctor', 'health'];
+let types = ['accounting','airport','amusement_park','aquarium','art_gallery','atm','bakery','bank','bar','beauty_salon','bicycle_store','book_store','bowling_alley','bus_station','cafe','campground','car_dealer','car_rental','car_repair','car_wash','casino','cemetery','church','city_hall','clothing_store','convenience_store','courthouse','dentist','department_store','doctor','drugstore','electrician','electronics_store','embassy','fire_station','florist','funeral_home','furniture_store','gas_station','gym','hair_care','hardware_store','hindu_temple','home_goods_store','hospital','insurance_agency','jewelry_store','laundry','lawyer','library','light_rail_station','liquor_store','local_government_office','locksmith','lodging','meal_delivery','meal_takeaway','mosque','movie_rental','movie_theater','moving_company','museum','night_club','painter','park','parking','pet_store','pharmacy','physiotherapist','plumber','police','post_office','primary_school','real_estate_agency','restaurant','roofing_contractor','rv_park','school','secondary_school','shoe_store','shopping_mall','spa','stadium','storage','store','subway_station','supermarket','synagogue','taxi_stand','tourist_attraction','train_station','transit_station','travel_agency','university','veterinary_care','zoo','health'];
 let typesCnt = 0;
 let config = {
     method: 'get',
@@ -24,8 +23,8 @@ runApi(0, types[typesCnt]);
 
 function runApi(c, type) {
     if(c >= apiMaxPages) {
-        writeFile(`data/places-${type}-${getRnd(999)}.csv`, finalResult); //Save all results to csv file
-        //writeFile(`data/places-${type}-${getRnd(999)}.json`, JSON.stringify(finalResult)); //Save all results to json file
+        if(finalResult.length)
+            writeFile(`data/greenwood/places-${type}-${getRnd(999)}.csv`, finalResult); //Save all results to csv file  OLD//writeFile(`data/places-${type}-${getRnd(999)}.json`, JSON.stringify(finalResult)); //Save all results to json file
         typesCnt++; //Increase to next type and run again
         if(typesCnt < types.length) {
             finalResult = []; //Empty data array
